5855f5e60f380902837b47784724b244e2d152d7
arapbaeva <aiymvector@gmail.com>
2022-10-23 12:01:19 +0600

    Added 11(grid)

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,40 @@
 import React from 'react';
 import './App.css';
-import {Dialogs} from './components/Dialogs/Dialogs';
-import {Header} from "./components/Header/Header";
-import {Navbar} from "./components/Navbar/Navbar";
-import {Profile} from "./components/Profile/Profile";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import {addPost, DialogsType, MessageType, PostsType, updateNewPostText,} from "./State";
 
-
-type AppPropsType = {
-    dialogs: Array<DialogsType>
-    messages: Array<MessageType>
-    posts: Array<PostsType>
-    addPostCallBack: (postMessage: string) => void
-    newPostText: string
-    updateNewPostText:(newText: string) => void
-
-}
-
-
-export const App = (props: AppPropsType) => {
+const App = () => {
     return (
-
-        <BrowserRouter>
-            <div className='app-wrapper'>
-
-                <Header/>
-                <Navbar/>
-                <div className='app-wrapper-content'>
-                    <Routes>
-                        <Route path="dialogs/*" element={<Dialogs dialogs={props.dialogs} messages={props.messages}
-                        />}/>
-                        <Route path="profile/*"
-                               element={<Profile posts={props.posts} addPostCallBack={addPost} newPostText={props.newPostText} updateNewPostText={props.updateNewPostText}
-                               />}/>
-                    </Routes>
-                </div>
+        <div className="wrapper">
+            <header className="header">
+                <img
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5V7V2JqjXrsD--ZUnZAWUesPljlYPQiJC9Q&usqp=CAU"
+                    alt=""/>
+            </header>
+            <nav className="nav">
+                <div><a>Profile</a></div>
+                <div><a>Messages</a></div>
+                <div><a>News</a></div>
+                <div><a>Music</a></div>
+                <div><a>Settings</a></div>
+            </nav>
+            <div className="content">
+                <img
+                    src="https://images.unsplash.com/photo-1518289646039-3e6c87a5aaf6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fHBpbmslMjBhYnN0cmFjdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+                    alt=""/>
             </div>
-        </BrowserRouter>
-    )
-}
-export default App
+            <div>
+                <img src="https://images.unsplash.com/photo-1485178575877-1a13bf489dfe?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Z2lybCUyMGxvZ298ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"/>
+            </div>
+            <div>post
+            <div>newpost</div>
+            </div>
+            <div>
+                <div>p1</div>
+                <div>p2</div>
+            </div>
+        </div>
 
 
+    );
+};
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import state, {addPost, RootStateType} from './State'
-import {renderTree} from "./render";
 
+ReactDOM.render(
+    <App />,
+  document.getElementById('root')
+);
 
-
-renderTree(state)
