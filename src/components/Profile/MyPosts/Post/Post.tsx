71aecb197324d4b7a0bcb52c6d6ba41569990e66
arapbaeva <aiymvector@gmail.com>
2023-08-15 16:55:58 +0600

    updated styles

--- src/components/Profile/MyPosts/Post/Post.tsx
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -10,10 +10,10 @@ const Post = (props: PropsType) => {
 
     return (
         <div className={s.item}>
-            <img src='https://oper.ru/static/data/gallery/l1048753984.jpg'/>
+            <img className={s.image} alt="logo"src="https://images.unsplash.com/photo-1485178575877-1a13bf489dfe?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Z2lybCUyMGxvZ298ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"/>
             {props.message}
             <div>
-                <span>like</span> {props.likesCount}
+                <span>{String.fromCodePoint(0x1F497)}</span> {props.likesCount}
             </div>
         </div>
     )
